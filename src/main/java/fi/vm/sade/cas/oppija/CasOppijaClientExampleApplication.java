0146bb13614750eb00f7708139502128fa4f3670
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-03-25 07:33:00 +0200

    Add cas configuration

--- src/main/java/fi/vm/sade/cas/oppija/CasOppijaClientExampleApplication.java
+++ src/main/java/fi/vm/sade/cas/oppija/CasOppijaClientExampleApplication.java
@@ -1,9 +1,11 @@
 package fi.vm.sade.cas.oppija;
 
+import net.unicon.cas.client.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCasClient
 public class CasOppijaClientExampleApplication {
 
     public static void main(String[] args) {
