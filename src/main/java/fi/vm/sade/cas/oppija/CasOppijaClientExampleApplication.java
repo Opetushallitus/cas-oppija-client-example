d12516cc463f64ba8c1661dd3946131735180c24
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-03-25 07:10:21 +0200

    Add empty Spring Boot web application

---
+++ src/main/java/fi/vm/sade/cas/oppija/CasOppijaClientExampleApplication.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.cas.oppija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasOppijaClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasOppijaClientExampleApplication.class, args);
+    }
+
+}
